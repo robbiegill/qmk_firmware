ce9c4e6acfaf9d432c236c4b9d45da2cfa514a15
Robbie Gill <gill.rob@gmail.com>
2016-06-12 21:47:03 -0700

    update: - space cadet paren

--- keyboard/hhkb_qmk/keymaps/default.c
+++ keyboard/hhkb_qmk/keymaps/default.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP_JP(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_INS, KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
-    CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_GRV, KC_ENT,
-    SFT_T_LPRN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,    KC_UP,  KC_RSFT,
+    CTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_GRV, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,    KC_UP,  KC_RSFT,
     MO(HIDDEN),  KC_LCTL, KC_LALT, KC_LGUI, MO(HIDDEN),      KC_SPC ,  KC_BSPC, KC_AT, KC_RALT, M(MACRO_HID_OS),  KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12, ______,  KC_DEL,
     ______, ______, ______, KC_EQL, ______, ______, ______, ______, ______, ______, KC_PIPE, KC_LPRN, KC_RPRN,
     ______, KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR, ______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ______, ______, ______, ______,
-    SFT_T_RPRN, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_PGUP, ______,
+    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_PGUP, ______,
     ______, ______, ______, ______, ______,     KC_UNDS    ,  KC_ENT, M_EMAIL, ______, ______, KC_HOME, KC_PGDN, KC_END
   ),
 
@@ -134,8 +134,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t macro_id, uint8_t o
         layer_on(HIDDEN);
         layer_on(OS_LAYER);
       } else {
-        layer_off(HIDDEN);
         layer_off(OS_LAYER);
+        layer_off(HIDDEN);
       }
       break;
     }
