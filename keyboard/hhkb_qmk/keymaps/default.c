3a3111515901403792abb866dac62c322484d1ac
Robbie Gill <gill.rob@gmail.com>
2016-07-04 21:52:06 -0700

    match 5x13

--- keyboard/hhkb_qmk/keymaps/default.c
+++ keyboard/hhkb_qmk/keymaps/default.c
@@ -5,21 +5,18 @@
 #include "timer.h"
 #include "action_layer.h"
 
-#define BASE 0
-#define HIDDEN 1
-#define OS_LAYER 2
-#define EMPTY 3
+#define _______ KC_TRNS
 
-#define ______ KC_TRNS
+enum {
+  ZER,
+  HDN,
+  OSY
+};
 
-#define MACRO_SFT_T_LPRN 0
-#define MACRO_SFT_T_RPRN 1
-#define MACRO_EMAIL 2
-#define MACRO_HID_OS 3
 
-#define SFT_T_LPRN M(MACRO_SFT_T_LPRN)
-#define SFT_T_RPRN M(MACRO_SFT_T_RPRN)
-#define M_EMAIL M(MACRO_EMAIL)
+#define CTL_ESC CTL_T(KC_ESC)
+#define SFT_BSP SFT_T(KC_BSPC)
+
 #define SCRNS3 LGUI(LCTL(LSFT(KC_3)))
 #define SCRNS4 LGUI(LCTL(LSFT(KC_4)))
 
@@ -39,107 +36,34 @@
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = KEYMAP_JP(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_INS, KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
-    CTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_GRV, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,    KC_UP,  KC_RSFT,
-    MO(HIDDEN),  KC_LCTL, KC_LALT, KC_LGUI, MO(HIDDEN),      KC_SPC ,  KC_BSPC, KC_AT, KC_RALT, M(MACRO_HID_OS),  KC_LEFT, KC_DOWN, KC_RGHT
+  [ZER] = KEYMAP_JP(
+    KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_INS,  KC_BSPC,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,
+    CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_GRV, KC_ENT,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,   KC_UP, KC_RSFT,
+    MO(HDN),  KC_LCTL, KC_LALT, KC_LGUI, MO(HDN),      KC_SPC,    SFT_BSP,  MO(HDN), MO(OSY),   KC_NO, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [HIDDEN] = KEYMAP_JP(
-    KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12, ______,  KC_DEL,
-    ______, ______, ______, KC_EQL, ______, ______, ______, ______, ______, ______, KC_PIPE, KC_LPRN, KC_RPRN,
-    ______, KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR, ______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_PGUP, ______,
-    ______, ______, ______, ______, ______,     KC_UNDS    ,  KC_ENT, M_EMAIL, ______, ______, KC_HOME, KC_PGDN, KC_END
+  [HDN] = KEYMAP_JP(
+    KC_GRV,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DEL,
+    _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______,
+    _______, KC_TILD,  KC_GRV, KC_BSLS, KC_PIPE, KC_MINS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_TILD,  KC_GRV, _______, _______,
+    _______, KC_VOLD, KC_VOLU, KC_MUTE,  KC_PWR, _______, _______,  KC_ENT, _______, _______, _______, _______, KC_PGUP, _______,
+    _______, _______, _______, _______, _______,     KC_UNDS     ,  KC_DEL, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
   ),
 
-  [OS_LAYER] = KEYMAP_JP(
-    ______, ______, ______, SCRNS3, SCRNS4, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______,     ______    , ______, ______, ______, ______, ______, ______, ______
-  ),
-
-  [EMPTY] = KEYMAP_JP(
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
-    ______, ______, ______, ______, ______,     ______    , ______, ______, ______, ______, ______, ______, ______
+  [OSY] = KEYMAP_JP(
+    _______, _______, _______,  SCRNS3,  SCRNS4, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,      _______    , _______, _______, _______, _______, _______, _______, _______
   )
 };
 
 static uint16_t key_timer;
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t macro_id, uint8_t opt) {
-  switch (macro_id) {
-    case MACRO_SFT_T_LPRN: {
-      if (record -> event.pressed) {
-        key_timer = timer_read();
-        register_code(KC_LSFT);
-      } else {
-        if (timer_elapsed(key_timer) < TAPPING_TERM) {
-          register_code(KC_9);
-          unregister_code(KC_9);
-        }
-        unregister_code(KC_LSFT);
-      }
-      break;
-    }
-    case MACRO_SFT_T_RPRN: {
-      if (record -> event.pressed) {
-        key_timer = timer_read();
-        register_code(KC_LSFT);
-      } else {
-        if (timer_elapsed(key_timer) < TAPPING_TERM) {
-          register_code(KC_0);
-          unregister_code(KC_0);
-        }
-        unregister_code(KC_LSFT);
-      }
-      break;
-    }
-    case MACRO_EMAIL: {
-      if (record -> event.pressed) {
-        return MACRO(
-          I(7),
-          T(R),
-          T(O),
-          T(B),
-          T(B),
-          T(I),
-          T(E),
-          D(LSFT),
-          T(AT),
-          U(LSFT),
-          T(G),
-          T(U),
-          T(S),
-          T(T),
-          T(O),
-          T(DOT),
-          T(C),
-          T(O),
-          T(M),
-          END
-        );
-      }
-      break;
-    }
-    case MACRO_HID_OS: {
-      if (record -> event.pressed) {
-        layer_on(HIDDEN);
-        layer_on(OS_LAYER);
-      } else {
-        layer_off(OS_LAYER);
-        layer_off(HIDDEN);
-      }
-      break;
-    }
-  }
   return MACRO_NONE;
 }
 
